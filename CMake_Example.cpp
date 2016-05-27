48f821830102d732170e8ea1dd6b1d5b63fde593
mbcslava <mbcslava@outlook.com>
2016-05-27 17:21:47 +0300

    Add files via upload

---
+++ CMake_Example.cpp
@@ -0,0 +1,15 @@
+// CMake_Exemple.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+	cout << "Hello, World!" <<endl;
+	return 0;
+	system("pause");
+}
+
